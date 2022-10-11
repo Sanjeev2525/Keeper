244d5921ce136d0f47b17b0b66d34e10bbb48b20
Sanjeev <sanjeevganesh25@gmail.com>
2022-10-11 12:38:55 +0530

    Used React map and fat arrow function

--- src/components/app.js
+++ src/components/app.js
@@ -2,12 +2,20 @@ import React from "react";
 import Header from "./header";
 import Footer from "./footer";
 import Note from "./note";
+import notes from "../notes";
 
 function App() {
 	return(
 		<div>
 		<Header />
-		<Note />
+		{notes.map((noteitem) =>{return(
+		<Note 
+			key={noteitem.key}
+			title={noteitem.title}
+			content={noteitem.content}/>
+			); 
+		})}
+
 		<Footer />
 		</div>);}
 
